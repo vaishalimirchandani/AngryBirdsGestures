b95cc5863a539cd03776dfeaca51c6992cd47e7f
vaishalimirchandani <vaishali.mirchandani@gmail.com>
2015-06-05 04:31:11 +0200

    Added: PaabolicTrajecttoryModel and ParabolicTrajectoryView Also added Luke and Vader picture

---
+++ AngryBirds/ParabolicTrajectoryModel.swift
@@ -0,0 +1,75 @@
+//
+//  ParabolicTrajectoryModel.swift
+//  AngryBirds
+//
+//  Created by Vaishali Mirchandani on 5/6/15.
+//  Copyright (c) 2015 UPM. All rights reserved.
+//
+
+import Foundation
+
+let EARTH_GRAVITY = 9.8067
+
+class TrajectoryModel {
+    
+    private var speedXaxis = 100.0
+    private var speedYaxis = 100.0
+    
+    var objective = 100.0
+    
+    var speed: Double {
+        get {
+            return sqrt(speedXaxis*speedXaxis + speedYaxis*speedYaxis)
+        }
+        set {
+            let a = angle
+            speedXaxis = newValue * cos(angle)
+            speedYaxis = newValue * sin(angle)
+        }
+    }
+    
+    var angle: Double {
+        get {
+            return atan2(speedYaxis, speedXaxis)
+        }
+        set {
+            let s = speed
+            speedXaxis = s * cos(newValue)
+            speedYaxis = s * sin(newValue)
+        }
+    }
+    
+    init() {
+    }
+    
+    init(speed:Double, angle:Double){
+        speedXaxis = speed * cos(angle)
+        speedYaxis = speed * sin(angle)
+    }
+    
+    func height(time: Double) -> Double {
+        return speedYaxis * time - 0.5 * time * time * EARTH_GRAVITY
+    }
+    
+    func distance(time: Double) -> Double {
+        return speedXaxis * time
+    }
+    
+    func position(time:Double)-> (distance:Double, height:Double){
+        return (distance(time), height(time))
+    }
+    
+    func duration() -> Double {
+        return (speedYaxis * 2)/EARTH_GRAVITY
+    }
+    
+    func distanceObjective()->Double {
+        return objective
+    }
+    
+    subscript (time:Double)-> (distance:Double, height:Double){
+        return position(time)
+    }
+    
+}
+
---
+++ AngryBirds/ParabolicTrajectoryView.swift
@@ -0,0 +1,99 @@
+//
+//  ParabolicTrajectoryView.swift
+//  AngryBirds
+//
+//  Created by Vaishali Mirchandani on 5/6/15.
+//  Copyright (c) 2015 UPM. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+protocol TrajectoryViewDataSource:class{
+    func startParabolicTrajectoryView () -> Double
+    func endParabolicTrajectoryView () -> Double
+    func pointOfParabolicTrajectoryView (time:Double) -> (distance:Double, height:Double)
+    func distanceToObjective() -> Double
+}
+
+@IBDesignable
+
+class TrajectoryView: UIView{
+    
+    var dataSource : TrajectoryViewDataSource!
+    
+    #if TARGET_INTERFACE_BUILDER
+        class FakeDataSource : TrajectoryViewDataSource {
+        
+            func startParabolicTrajectoryView() -> Double {
+                return 0.0
+            }
+            func endParabolicTrajectoryView() -> Double {
+                return 100.0
+            }
+            func pointOfParabolicTrajectoryView(time: Double) -> (distance: Double, height: Double) {
+                return(30.0,100.0)
+            }
+            func distanceToObjective()-> Double {
+                return 100.0
+            }
+        }
+        
+        override func prepareForInterfaceBuilder() {
+            strongDS=FakeDataSource()
+            dataSource = strongDS
+        }
+    #endif
+    
+    override func drawRect(rect: CGRect) {
+        if dataSource == nil {
+            return
+        }
+        
+        var path = UIBezierPath()
+        
+        let t_initial = dataSource.startParabolicTrajectoryView()
+        let t_final = dataSource.endParabolicTrajectoryView()
+        
+        var position = dataSource.pointOfParabolicTrajectoryView(t_initial)
+        
+        var x = position.distance
+        var y = position.height
+        
+        let h:Double = Double(bounds.size.height)
+        
+        
+        path.moveToPoint(CGPointMake(CGFloat(x+20) ,CGFloat(h-y)))
+        
+        for var t=t_initial ; t<t_final ; t+=0.1 {
+            let position = dataSource.pointOfParabolicTrajectoryView(t)
+            path.addLineToPoint(CGPointMake(CGFloat(position.distance+20),CGFloat(h-position.height)))
+        }
+        
+        path.lineWidth = 3
+        UIColor.redColor().set()
+        path.stroke()
+        drawLuke()
+        drawVader()
+    }
+    
+    
+    let IMG_SIZE = 50
+    
+    private func drawLuke(){
+        if let img = UIImage(named: "Luke.png"){
+            let y = bounds.size.height - CGFloat(IMG_SIZE)
+            let rect = CGRectMake(0, y, CGFloat(IMG_SIZE), CGFloat(IMG_SIZE))
+            img.drawInRect(rect)
+        }
+    }
+    
+    private func drawVader(){
+        if let img = UIImage(named: "Vader.png"){
+            let x = CGFloat(dataSource.distanceToObjective())
+            let y = bounds.size.height - CGFloat(IMG_SIZE)
+            let rect = CGRectMake(x, y, CGFloat(IMG_SIZE), CGFloat(IMG_SIZE))
+            img.drawInRect(rect)
+        }
+    }
+}
